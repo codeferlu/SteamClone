5c223b40239de10235a54e5fb31e4bf549133d5e
Fernando <codeferlu@gmail.com>
2024-06-15 16:22:49 -0400

    cambios

--- server/controllers/games.js
+++ server/controllers/games.js
@@ -2,13 +2,11 @@ const pool = require('../config/database');
 
 const getAllGames = async (req, res) => {
   try {
-    const client = await pool.connect();
-    const result = await client.query('SELECT * FROM games');
-    client.release();
+    const result = await pool.query('SELECT * FROM games');
     res.json(result.rows);
   } catch (error) {
     console.error('Error getting games:', error);
-    res.status(500).json({ message: 'Internal Server Error' });
+    res.status(500).json({ error: 'Internal Server Error' });
   }
 };
 
