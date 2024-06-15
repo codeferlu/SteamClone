9a83748e5eded61c5762d269176335617dbdb6db
Fernando <codeferlu@gmail.com>
2024-06-15 14:57:39 -0400

    GameList.jsx

---
+++ client/src/components/GameList.jsx
@@ -0,0 +1,40 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import { Link } from 'react-router-dom';
+
+const GameList = () => {
+  const [games, setGames] = useState([]);
+
+  useEffect(() => {
+    const fetchGames = async () => {
+      const response = await axios.get('/api/games');
+      setGames(response.data);
+    };
+
+    fetchGames();
+  }, []);
+
+  return (
+    <div className="container">
+      <h2>Games</h2>
+      <div className="row">
+        {games.map((game) => (
+          <div className="col-md-4" key={game.id}>
+            <div className="card mb-4">
+              <img src={game.imageUrl} className="card-img-top" alt={game.title} />
+              <div className="card-body">
+                <h5 className="card-title">{game.title}</h5>
+                <p className="card-text">{game.description}</p>
+                <Link to={`/games/${game.id}`} className="btn btn-primary">
+                  View Details
+                </Link>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default GameList;
